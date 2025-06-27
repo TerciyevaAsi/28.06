20ec49855109a8d2c509fceadaa3b64bf3707f94
Asiyyat <asiyyatterciyeva@gmail.com>
2025-06-27 14:45:23 +0000

    first

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+let studentScore =  prompt("Adınız:")
+if (studentScore >= 81) {
+console.log(5)
+} else if (studentScore >= 61) {
+console.log(4)
+} else if  (studentScore >= 31) {
+console.log(3)
+} else {
+console.log(2)
+}
